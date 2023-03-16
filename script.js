b880d512e0bb86dd90e7e04ac31a6d3f36e4c6be
RAVI <124796706+Rajasthanichora@users.noreply.github.com>
2023-03-16 17:17:24 +0530

    all updated

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ inches.addEventListener("input", function () {
   }
   yards.value = jhh;
 });
+
+// YARD BUTTON
+
+function unitbtn() {
+  var x = document.getElementById("slidedropef");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  // close inch buttun
+  var xvxgg = document.getElementById("slidedropeff");
+  xvxgg.style.display = "none";
+}
+
+// INCH BUTTON
+
+function unitbtnn() {
+  var xvx = document.getElementById("slidedropeff");
+  if (xvx.style.display === "none") {
+    xvx.style.display = "block";
+  } else {
+    xvx.style.display = "none";
+  }
+  // close inch buttun
+  var ffe = document.getElementById("slidedropef");
+  ffe.style.display = "none";
+}
